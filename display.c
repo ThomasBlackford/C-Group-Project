f83c19d6a12da81f835b2306bc9022afc3b2f095
kwahab1 <160540820+kwahab1@users.noreply.github.com>
2024-03-01 08:59:58 -0500

    Update display.c
    im not done yet everyone

--- display.c
+++ display.c
@@ -1,1 +1,26 @@
+#include <gtk/gtk.h>
 
+int main(int argc, char *argv[]) {
+    // Initialize GTK
+    gtk_init(&argc, &argv);
+
+    // Create a new window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello World");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+    // Set an app icon
+    GdkPixbuf *icon = gdk_pixbuf_new_from_file("app-icon.png", NULL);
+    gtk_window_set_icon(GTK_WINDOW(window), icon);
+
+    // Connect the "destroy" signal of the window to gtk_main_quit
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Show the window
+    gtk_widget_show_all(window);
+
+    // Start the GTK main loop
+    gtk_main();
+
+    return 0;
